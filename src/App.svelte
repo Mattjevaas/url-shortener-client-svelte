0bd7aa1ca0bef487ed06a77fbb93889382061962
Mattjevaas <johaneswiku@protonmail.ch>
2022-09-10 15:52:59 +0700

    creating url shortener main interface

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  import Urlinput from './lib/Urlinput.svelte'
+</script>
+
+<main>
+  <h1>URL Shortener</h1>
+
+  <div>
+    <Urlinput />
+  </div>
+
+  <p>
+    Check out my <a href="https://github.com/sveltejs/kit#readme" target="_blank"> Github </a>repositories
+  </p>
+
+  <p class="read-the-docs">
+  </p>
+</main>
+
+<style>
+  /* .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  } */
+</style>
---
+++ src/lib/Urlinput.svelte
@@ -0,0 +1,71 @@
+<script>
+    let value = ""
+
+    async function createShortUrl() {
+		const res = await fetch(`http://127.0.0.1:8080/save`,{
+            method: 'POST',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({original_url: value})
+        });
+
+		const response = await res.json();
+
+        //console.log(response)
+
+		if (response.code == 200) {
+			return `johaneswiku.com/${response.data.short_url}`;
+		} else {
+			throw new Error(response.status);
+		}
+	}
+	
+	let promise = null
+
+	function handleClick() {
+		promise = createShortUrl();
+	}
+</script>
+
+
+<div class="input-wrapper">
+    <input type="text" class="input-area" placeholder="https://example.com" bind:value>
+    <div class="spacer"/>
+    <button class="button-size" on:click={handleClick}>
+        Short!
+    </button>
+</div>
+
+{#if promise != null}
+    {#await promise}
+        <p>creating URL ...</p>
+    {:then value}
+        <p>Your Short URL:  <a href={value} target="_blank" >{value}</a></p>
+    {:catch error}
+        <p style="color: red">{error.message}</p>
+    {/await}
+{/if}
+
+<style>
+    .input-wrapper{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .input-area{
+        min-width: 30vw;
+        min-height: 5vh;
+        padding: 8px;
+        font-size: 20px;
+    }
+    .spacer{
+        width: 10px;
+    }
+    .button-size{
+        min-height: 6vh;
+        min-width: 5vw;
+        padding: 8px;
+    }
+</style>
